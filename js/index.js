3bd426830983b47e90e0237f3644603355aa2434
esteban <esteban.quinones@outlook.fr>
2022-01-25 16:51:05 +0100

    indexV2

--- js/index.js
+++ js/index.js
@@ -14,13 +14,3 @@ $('a[href*=#]:not([href=#])').click(function() {
     }
 });
 
-let sourceSwap = function () {
-    let $this = $(this);
-    let newSource = $this.data('alt-src');
-    $this.data('alt-src', $this.attr('src'));
-    $this.attr('src', newSource);
-}
-
-$(function () {
-    $('img.eyes').hover(sourceSwap, sourceSwap);
-});
