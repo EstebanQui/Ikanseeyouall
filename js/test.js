511048f7924361da756e462b633c3a2f33731348
EstebanQui <74357764+EstebanQui@users.noreply.github.com>
2022-02-13 23:47:29 +0100

    Update test.js

--- js/test.js
+++ js/test.js
@@ -3,6 +3,7 @@ design by Voicu Apostol.
 design: https://dribbble.com/shots/3533847-Mini-Music-Player
 I can't find any open music api or mp3 api so i have to download all musics as mp3 file.
 You can fork on github: https://github.com/muhammederdem/mini-player
+https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg
 */
 
 new Vue({
